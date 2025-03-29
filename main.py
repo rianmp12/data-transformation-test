a9b837d56432d10543fcf2c3c886d8df2805ff85
rianmp12 <rianmp12@gmail.com>
2025-03-28 23:38:21 -0300

    Add processamento de PDF com limpeza de dados e compactação

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-# TODO: implementar extração de colunas específicas
+import pdfplumber
+import pandas as pd
+import os
+import zipfile
+
+def extract_tables_from_pdf(pdf_file):
+    todas_tabelas = []
+
+    with pdfplumber.open(pdf_file) as pdf:
+        for pagina in pdf.pages:
+            tabelas = pagina.extract_tables()
+            for tabela in tabelas:
+                df = pd.DataFrame(tabela[1:], columns=tabela[0])
+                todas_tabelas.append(df)
+
+    if todas_tabelas:
+        return pd.concat(todas_tabelas, ignore_index=True)
+    else:
+        return pd.DataFrame()
+
+def replace_abbreviations_and_clean(df):
+    # Substituições no OD e AMB
+    for col in df.columns:
+        df[col] = df[col].replace("OD", "Consultório")
+        df[col] = df[col].replace("AMB", "Ambulatorial")
+
+    # Limpa quebras de linha nas células
+    df = df.astype(str).apply(lambda col: col.str.replace('\n', ' ', regex=False))
+
+    # Limpa e normaliza os nomes das colunas
+    df.columns = [col.replace('\n', ' ').replace("á", "a").strip() for col in df.columns]
+
+    return df
+
+def save_and_compress_csv(df, csv_file, zip_file):
+    os.makedirs(os.path.dirname(csv_file), exist_ok=True)
+    df.to_csv(csv_file, index=False, encoding='utf-8-sig')
+
+    with zipfile.ZipFile(zip_file, "w") as zipf:
+        zipf.write(csv_file, arcname=os.path.basename(csv_file))
+
+    print(f"CSV saved and compressed at: {zip_file}")
+
+if __name__ == "__main__":
+    pdf_path = "./downloads/attachment_1.pdf"  # Anexo I
+    csv_path = "./csv_path/table_rol.csv"
+    zip_path = "./output/Teste_Rian.zip"
+    df_final = extract_tables_from_pdf(pdf_path)
+
+    if not df_final.empty:
+        df_final = replace_abbreviations_and_clean(df_final)
+        save_and_compress_csv(df_final, csv_path, zip_path)
+    else:
+        print("No table was found in the PDF.")
